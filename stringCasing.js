3d6f7dad137e0a7c640b3a785935fff6ecbcc159
Durga Sumesh <118443621+aaagrud@users.noreply.github.com>
2024-01-28 10:47:21 +0530

    Update stringCasing.js

--- stringCasing.js
+++ stringCasing.js
@@ -4,4 +4,4 @@ goal: to capitalize just the first letter of a name entered by user
 */
 
 var name = prompt("enter name: ");
-alert("Hello " + name.slice(0, 1).toUpperCase() + name.slice(1, name.length));
+alert("Hello " + name.slice(0, 1).toUpperCase() + name.slice(1, name.length).toLowerCase());

12ded4fcab58fcfc6e304c2ac1632702cb3f5780
Durga Sumesh <118443621+aaagrud@users.noreply.github.com>
2024-02-09 15:05:38 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,23 @@
+/*
+functions lets us reuse the same code a bunch of times.
+saves our time!
+
+syntax:
+function functionname(){
+    definition
+}
+
+we can use the function as many times as we need by calling it.
+we call it using the function's name
+*/
+
+function goodDay(){
+    alert("good day!");
+}
+
+goodDay();
+goodDay();
+goodDay();
+goodDay();
+goodDay();
+goodDay();

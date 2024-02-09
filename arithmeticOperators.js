fc27075c5f86ed70e6c8dbfe76a817804ba10905
Durga Sumesh <118443621+aaagrud@users.noreply.github.com>
2024-02-09 14:46:27 +0530

    Create arithmeticOperators.js

---
+++ arithmeticOperators.js
@@ -0,0 +1,7 @@
+/* human age to dog age converter!
+Use arithmetic operators in javascript and understand precedence to accomplish this
+*/
+
+
+var age = prompt("enter your age: ");
+alert("You are " + (((age - 21)/4) + 2) + " years old in dogs years!");

02309ae156d91b0abdb43a31f044f5be7a99d1a2
Durga Sumesh <118443621+aaagrud@users.noreply.github.com>
2024-02-09 14:52:19 +0530

    Create incrementDecrement.js

---
+++ incrementDecrement.js
@@ -0,0 +1,18 @@
+/*
+x++ means x = x + 1
+x-- means x = x - 1
+
+similarily, 
+x += y means x = x + y
+x /= y means x = x / y
+x *= y means x = x * y
+x -= y means x = x - y
+and so on
+
+*/
+
+var number = prompt("enter a number: ");
+number++;
+alert("your number is now: " + number);
+number--;
+alert("your number is now back to: " + number);
